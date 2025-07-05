a76aa8db2c1c8a4dc9997f7b77c1d25f5bfcf04f
Nova <nova@cutey.com>
2025-07-05 18:53:17 +0100

    fuck im burnt out, this code makes no sense

--- tools/internal/parsing.py
+++ tools/internal/parsing.py
@@ -39,26 +39,27 @@ class Parser:
 
     def ReadLevel(self, level):
         js = self.grabLevel(level)
-        level = Level(js)
+        level = Level(level, js)
 
 
 
 
 class Level:
-    def __init__(self, js):
+    def __init__(self, levelName, js):
         self.rooms = []
         for room in js["level"]["rooms"]:
             try:
-                temp = Room(room["ID"], room["exits"], room["entities"])
+                temp = Room(levelName, room["ID"], room["exits"], room["entities"])
                 self.rooms.append(temp)
                 #print(temp)
             except (KeyError, IndexError) as e:
                 logger.warning("room " + room["ID"] + " has errors at index " + str(e))
+                raise e
     def buildLevel(self):
         pass
 
 class Room:
-    def __init__(self, id, exits, entities):
+    def __init__(self, level, id, exits, entities):
         self.ID = id
         self.exits = exits,
         self.entities = entities
@@ -69,18 +70,31 @@ class Room:
             "west": []
         }
 
-        self.buildRoom()
+        self.buildRoom(level)
+        print(self.actualEntities)
 
-    def buildRoom(self):
-        temp = self.entities[0].split("/")
-        #print(temp)
-        file = open(_levelPath + "levels/" + temp[0] + ".json")
+    def buildRoom(self, level):
+        file = open(_levelPath + "levels/" + level + ".json")
         js = json.load(file)
         file.close()
-        for i in range(len(js)):
-            print(js[i])
-            if js[i].location == "north" or js[i].location == "n":
-                self.actualEntities["north"].append(js[i])
+        file = open(_entityPath + "entities/" + level + ".json")
+        entjs = json.load(file)
+        file.close()
+        for i in js["level"]["rooms"]:
+            for entity in i["entities"]:
+                for ent in entjs:
+                    temp = entity.split("/")
+                    if temp[-1] in ent.values():
+                        if ent["type"] == "generic:spawnPoint":
+                            pass
+                        elif ent["location"] == "north" or ent["location"] == "n":
+                            self.actualEntities["north"].append(ent)
+                        elif ent["location"] == "east" or ent["location"] == "e":
+                            self.actualEntities["east"].append(ent)
+                        elif ent["location"] == "south" or ent["location"] == "s":
+                            self.actualEntities["south"].append(ent)
+                        elif ent["location"] == "west" or ent["location"] == "w":
+                            self.actualEntities["west"].append(ent)
 
 
 
@@ -94,9 +108,10 @@ class Entity:
     def __init__(self, ID, js):
         self.ID = ID
         try:
+            if
             self.location = js["location"]
         except (KeyError, IndexError):
-            logger.warning(f"invalid entity at {js["id"]}")
+
         print(f"{self.ID}, {self.location}")
 
 parser = Parser("levels/levels", "entities/entities", ["entities/generic", "levels/generic"])
